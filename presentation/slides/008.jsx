b4bd09f71b012b2fb90f657d9e03d520c2609e47
Jesse Buchanan <hello@jessebuchanan.ca>
2016-03-08 16:30:06 -0500

    add TC39 links

--- presentation/slides/008.jsx
+++ presentation/slides/008.jsx
@@ -30,8 +30,8 @@ const slide = (
       <ListItem>There are some experimental features popular with React developers that are in the various stages:</ListItem>
       <List textColor="tertiary" textSize="0.75em" margin="20px">
         <ListItem><a href="https://github.com/sebmarkbage/ecmascript-rest-spread">Rest/Spread Properties (Stage 2)</a></ListItem>
-        <ListItem>Class and Property Decorators (Stage 1)</ListItem>
-        <ListItem>Class Fields & Static Properties (Stage 1)</ListItem>
+        <ListItem><a href="https://github.com/wycats/javascript-decorators/blob/master/README.md">Class and Property Decorators (Stage 1)</a></ListItem>
+        <ListItem><a href="https://github.com/jeffmo/es-class-fields-and-static-properties">Class Fields & Static Properties (Stage 1)</a></ListItem>
       </List>
     </List>
   </Slide>
